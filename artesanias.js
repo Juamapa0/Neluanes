f4a5600d67ac734dd34b7147dd11dba62de4c394
juamapa0 <juamapa0@gmail.com>
2023-09-01 14:09:23 -0300

    carpeta nueva

--- artesanias.js
+++ artesanias.js
@@ -3,23 +3,34 @@ const cardsContainer = document.querySelector('.cards-container');
 const productList = [];
 // agrego elementos al array
 productList.push({
+    file:'pulsera_plata.html',
     name:'Pulsera',
     detalle:'Diseño salteño',
     price: 'Desde:$1000',
     image:'https://lh3.googleusercontent.com/pw/AM-JKLVmrCyfk1AVQuUROgQybtRTAK4JJZqKFwTICJJ0mJDYyd622LFoW7zkioWd7us6eObCuLjGOA5vxjByTPmtUsWW8viG7Zrj0IIyTtzhLiitKZpXezJozCvi14JunJ3HAELIRtkSrAdBKkG4v9gFSIPvbA=s720-no?authuser=0', 
 });
 productList.push({
+    file: 'pulsera_alpaca.html',
     name:'Pulsera',
     detalle:'Diseño salteño',
     price:'Desde:$800',
     image:'https://lh3.googleusercontent.com/pw/AM-JKLVSWPbw1xY4NiJZO3Ccj4MYNxg_o0voUWIOqU9OW6GzwNwxBTa1xbY1YCjWvnpMuddrtp5aEYFrRcMXRQCpa5ce7wb6yvc7eS-qMwk2PmP3Yht2JLWmjiJzsFo-j7SDYY3ErxouDKJPb4i8P3igXHaEFA=w720-h341-no?authuser=0', 
 });
 productList.push({
+    file: 'aros_alpaca.html',
     name:'Aro',
     detalle:'Diseño salteño',
     price:'Desde:$100',
     image:'https://lh3.googleusercontent.com/pw/AM-JKLUmVq6IPaV0EM0_YtF6Pbu322YPQ29SHfUvsVckbWECa36xYxIMXHrOwkQ0f-l6n5Di9Xm2ixlmUxAnYLX62RFFa4Yq7LmziBD3EOgX8VaZOacIeJ_a1hAmPuTiQFMRV2BEzdny3pH4k5-mUPkbdzNPxA=w720-h341-no?authuser=0', 
 });
+productList.push({
+  file: 'aros_plastico.html',
+  name:'Aro plastico',
+  detalle:'Diseño salteño',
+  price:'Desde:$100',
+  image:'https://lh3.googleusercontent.com/pw/AM-JKLUmVq6IPaV0EM0_YtF6Pbu322YPQ29SHfUvsVckbWECa36xYxIMXHrOwkQ0f-l6n5Di9Xm2ixlmUxAnYLX62RFFa4Yq7LmziBD3EOgX8VaZOacIeJ_a1hAmPuTiQFMRV2BEzdny3pH4k5-mUPkbdzNPxA=w720-h341-no?authuser=0', 
+});
+
 
 /*<div class="cards-container">      
     <div class="product-card">
@@ -45,7 +56,7 @@ for (product of productList){
   // crea un elemento <a>
   const productA = document.createElement('a');
   // agrega href  y ./ a la etiqueta <a>
-  productA.setAttribute('href', './index.html');
+  productA.setAttribute('href', './artes/'+ product.file);
   // crea  un elemento img
   const productImg = document.createElement('img');
   // agrega el src y el product.image ¿?
